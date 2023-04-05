57f576d4f22f68f77bb9c61f59166a2168dbd320
Seyi-Banjo <elijahbanjo@gmail.com>
2023-04-06 00:44:21 +0100

    Admin Views

---
+++ app/admin_views.py
@@ -0,0 +1,9 @@
+from app import app
+
+@app.route('/')
+def index():
+    return "<h1>Hi, Dad</h1>"
+
+@app.route('/about')
+def about():
+    return "<div style='color': blue  > Hi, Elijah here! </div>"
